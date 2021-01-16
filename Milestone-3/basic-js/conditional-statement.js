5d643771014a01e81543fe3c4c795a26b885e350
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-01-16 12:53:03 +0600

    end module-1

---
+++ Milestone-3/basic-js/conditional-statement.js
@@ -0,0 +1,13 @@
+var jobPaiso = false;
+var betonKoto = 8000;
+var savingMoney = 3000;
+
+if(betonKoto >= 10000 && jobPaiso == true) {
+    console.log("I agree to marry you");
+}
+else if(savingMoney==100000) {
+    console.log("Vebe Dekhi")
+}
+else {
+    console.log("Hobe na. Job Manage koro age.");
+}
--- Milestone-3/basic-js/number.js
+++ Milestone-3/basic-js/number.js
@@ -16,3 +16,16 @@ var number3 = 8.001;
 var numberRound = Math.round(number3);
 console.log(numberRound);
 
+//Math ceil
+
+var number4 = 5.00000009;
+var ceilNumber = Math.ceil(number4);
+console.log(ceilNumber);
+
+//Math fllor
+
+var number5 = 7.99999999;
+var floorNumber = Math.floor(number5);
+console.log(floorNumber);
+
+
