aeea82d065c6bcf97d129407a823cc80571709e6
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-02-03 16:05:25 +0600

    constructor()

---
+++ Milestone-5/es6-practice/constructor.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(sName, sId, sGpa){
+        this.name = sName;
+        this.id = sId;
+        this.gpa = sGpa;
+    }
+}
+const student1 = new Student('Mojnu', 180113, 5.00);
+const student2 = new Student('Mominul', 462368, 5.00);
+const student3 = new Student('Asad', 101010, 4.94);
+console.log(student1);
